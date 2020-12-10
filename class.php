4cb974136d0a8732ac05f5429a3b451191725787
Shivam Nerwal <60189715+shivamnerwal1998@users.noreply.github.com>
2020-12-10 10:31:14 +0530

    abstract_class_php_10/12

--- class.php
+++ class.php
@@ -1,18 +1,21 @@
-<?php
-   class vehicle {
-      function __call($name,$arg){
-         if($name == 'name')
-            if( count($arg) == 2) {
-               echo "it is a two weeler <br> " ;
-            }
-            else if( count($arg) == 4 ){
-              echo "it is a four wheeler <br>" ;
-            }
-            
-      }
-   }
-   $bike = new vehicle;
-   $bike->name(1,1);
-   $car = new vehicle;
-   $car->name(1,1,1,1);
-?>
+
+<?php 
+  abstract class A { 
+      function __construct() { 
+          echo " this is parent class  "; 
+      }  
+      abstract function printdata(); 
+  } 
+  class Derived extends A { 
+      function __construct() { 
+          echo " Derived class constructor <br>"; 
+      } 
+      function printdata() { 
+          echo " Derived class printdata function <br>"; 
+      } 
+      
+  } 
+  $obj = new Derived; 
+  $obj->printdata();
+ 
+  ?> 
